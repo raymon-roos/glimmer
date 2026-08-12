a40ef7bea7ee707b6d4456575a68f25f29a9019b
Raymon Roos <raymon.roos@hotmail.com>
2026-08-12 14:25:13 +0200

    chore: initial nix flake for gleam dev shell

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    allSystems = ["x86_64-linux"];
+
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs allSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forAllSystems ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          gleam
+          beamMinimal28Packages.erlang
+        ];
+      };
+    });
+  };
+}
